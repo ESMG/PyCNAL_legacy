f928e2b8812b9f7908b6e255f348127f84d18c85
Kate Hedstrom <kshedstrom@alaska.edu>
2016-07-05 13:14:42 -0800

    Exploring the __init__ interaction with *.so.

--- pyroms_toolbox/pyroms_toolbox/__init__.py
+++ pyroms_toolbox/pyroms_toolbox/__init__.py
@@ -47,8 +47,8 @@ from .smooth_1D import smooth_1D
 from . import BGrid_SODA
 from .get_littoral import get_littoral
 from .get_littoral2 import get_littoral2
-from _move_runoff import move_runoff
-from _move_river_t import move_river_t
+from ._move_runoff import move_runoff
+from ._move_river_t import move_river_t
 from .TS_diagram import TS_diagram
 from .date2jday import date2jday
 from .jday2date import jday2date
