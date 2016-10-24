41d238364d84946da8af142b4b8c334572763de1
Bryan Raney <bryan.raney@rutgers.edu>
2016-10-24 03:18:38 -0400

    Added comments to explain the grid conversion steps

--- mom6_tools/grid_converter/CreateFMSgridTopo.py
+++ mom6_tools/grid_converter/CreateFMSgridTopo.py
@@ -5,6 +5,50 @@ import numpy
 import netCDF4
 import Spherical
 
+# This script converts a ROMS horizontal grid into a MOM6 horizontal
+# grid.
+
+# Both ROMS and MOM6 horizontal grids use an Arakawa C-grid, with four
+# types of points:
+#   rho: the centers of the cells
+#   psi: the corners of the cells, located diagonally between the
+#        'rho' points
+#   u:   the u-velocity points, located between 'rho' points in the
+#        east/west direction
+#   v:   the v-velocity points, located between 'rho' points in the
+#        north/south direction
+
+# The main differences between the two grids are:
+#  * the outermost points of the ROMS grid are the 'rho' points, while
+#    the outermost points of the MOM6 grid are the 'psi' points (both
+#    with interspersed 'u' and 'v' points); and
+#  * the MOM6 grid interleaves all four types of points into a single
+#    "supergrid", while ROMS stores them as separate grids.
+
+# The ROMS grid looks like this, with an extra layer of 'rho' points
+# around the outside:
+# (see https://www.myroms.org/wiki/Numerical_Solution_Technique)
+#
+#       p - p - p - p - p
+#    3  | + | + | + | + |     p = rho (center) points
+#       p - p - p - p - p     + = psi (corner) points
+#    2  | + | + | + | + |     - = u points
+#       p - p - p - p - p     | = v points
+#    1  | + | + | + | + |
+#       p - p - p - p - p
+#
+#         1   2   3   4
+
+# The MOM6 grid has 'psi' points on the outside, not 'rho':
+#
+#    3    + | + | + | +       p = rho (center) points
+#         - p - p - p -       + = psi (corner) points
+#    2    + | + | + | +       - = u points
+#         - p - p - p -       | = v points
+#    1    + | + | + | +
+#
+#         1   2   3   4
+
 def read_ROMS_grid(roms_grid_filename):
     """Load the ROMS grid from a NetCDF file."""
 
