6715e9c52e76c32eab864c68eaf8a0af2de23b29
Bryan Raney <bryan.raney@rutgers.edu>
2016-10-24 00:19:29 -0400

    Add grid conversion verification tests
    Script to test that the python code generates the correct MOM6 grid
    from the CCS1 ROMS grid.

---
+++ mom6_tools/grid_converter/do_tests.bash
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+function ensure_nc_files_match() {
+    # Uses the "nccmp" tool to compare two NetCDF files and ensure the
+    # metadata and data values match. (See http://nccmp.sourceforge.net/)
+
+    NCFILE1="$1"
+    NCFILE2="$2"
+
+    if [ -z "${NCFILE1}" -o -z "${NCFILE2}" ] ; then
+        echo "Usage: $FUNCNAME NCFILE1 NCFILE2"
+        echo "Ensure the two NetCDF files match"
+        exit 1
+    fi
+
+    # Find all the metadata diffrences (except history)
+    nccmp --metadata --global --force "${NCFILE1}" "${NCFILE2}"
+    status=$?
+
+    # Indicate if the data differs, without printing out every different value
+    nccmp --data --Tolerance=1e-5 --statistics --force --quiet "${NCFILE1}" "${NCFILE2}"
+    status2=$?
+
+    # For nccmp, a return status of 0 means identical; 1 means not
+    # identical; >1 means error.  Since we ran it twice, return the
+    # largest status value.
+    if [ $status2 -gt $status ] ; then
+        status=$status2
+    fi
+
+    if [ $status -eq 0 ] ; then
+	echo "PASSED"
+    else
+	echo "FAILED ($status)"
+    fi
+    return $status
+}
+
+python CreateFMSgridTopo.py
+ensure_nc_files_match expected_output/ocean_hgrid.nc ocean_hgrid.nc
+ensure_nc_files_match expected_output/ocean_topog.nc ocean_topog.nc
