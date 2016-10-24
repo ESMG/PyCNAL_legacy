5372acafcd4c3edd62553b1fdf73991d3bc36d0f
Bryan Raney <bryan.raney@rutgers.edu>
2016-10-24 03:11:18 -0400

    Refactor: move more code into functions
    Moved the grid manipulation code into separate functions.  Grids are
    contained in dictionaries; functions return new/modified dictionaries.
    (almost object-oriented)

--- mom6_tools/grid_converter/CreateFMSgridTopo.py
+++ mom6_tools/grid_converter/CreateFMSgridTopo.py
@@ -25,57 +25,92 @@ def read_ROMS_grid(roms_grid_filename):
 
     return roms_grid
 
-# Open ROMS grid file
-roms_grid_filename = 'CCS_7k_0-360_fred_grd.nc'
-roms_grid = read_ROMS_grid(roms_grid_filename)
+def trim_ROMS_grid(old_grid):
+    """Remove extraneous points on the outside of the ROMS grid."""
 
-nj,ni = roms_grid['rho']['lon'].shape
-nj -= 2
-ni -= 2
+    trim_subgrid = dict()
+    # remove the outer:   ( rows,  cols)
+    trim_subgrid['psi'] = (False, False) # Cell corners (leave alone)
+    trim_subgrid['rho'] = ( True,  True) # Cell centers (remove outer row and column)
+    trim_subgrid[ 'u' ] = ( True, False) # U-points (remove outer row)
+    trim_subgrid[ 'v' ] = (False,  True) # V-points (remove outer column)
 
-# Supergrid shape: Smallest useful super-grid has a multiplier of 2
-snj = 2 * nj
-sni = 2 * ni
+    new_grid = dict()
+    for subgrid in old_grid.keys():
+        new_grid[subgrid] = dict()
+        trim_rows,trim_cols = trim_subgrid[subgrid]
+        for field in old_grid[subgrid].keys():
+            if trim_rows and trim_cols:
+                new_grid[subgrid][field] = old_grid[subgrid][field][1:-1,1:-1]
+            elif trim_rows:
+                new_grid[subgrid][field] = old_grid[subgrid][field][1:-1, :  ]
+            elif trim_cols:
+                new_grid[subgrid][field] = old_grid[subgrid][field][ :  ,1:-1]
+            else:
+                new_grid[subgrid][field] = old_grid[subgrid][field][ :  , :  ]
 
-# Declare shapes
-mom6_grid = dict()
-mom6_grid['supergrid'] = dict()
-mom6_grid['supergrid']['lon']   = numpy.zeros((snj+1,sni+1))
-mom6_grid['supergrid']['lat']   = numpy.zeros((snj+1,sni+1))
-mom6_grid['supergrid']['area']  = numpy.zeros((snj,  sni  ))
-mom6_grid['supergrid']['dx']    = numpy.zeros((snj+1,sni  ))
-mom6_grid['supergrid']['dy']    = numpy.zeros((snj,  sni+1))
-mom6_grid['supergrid']['angle'] = numpy.zeros((snj+1,sni+1))
+    return new_grid
 
-# Copy in data from ROMS file
-mom6_grid['supergrid']['lon'][ ::2, ::2] = roms_grid['psi']['lon'][ :  , :  ] # Cell corners
-mom6_grid['supergrid']['lon'][1::2,1::2] = roms_grid['rho']['lon'][1:-1,1:-1] # Cell centers (drop outside row and column)
-mom6_grid['supergrid']['lon'][1::2, ::2] = roms_grid['u'  ]['lon'][1:-1, :  ] # U-points (drop outside row)
-mom6_grid['supergrid']['lon'][ ::2,1::2] = roms_grid['v'  ]['lon'][ :  ,1:-1] # V-points (drop outside column)
-mom6_grid['supergrid']['lat'][ ::2, ::2] = roms_grid['psi']['lat'][ :  , :  ] # Cell corners
-mom6_grid['supergrid']['lat'][1::2,1::2] = roms_grid['rho']['lat'][1:-1,1:-1] # Cell centers (drop outside row and column)
-mom6_grid['supergrid']['lat'][1::2, ::2] = roms_grid['u'  ]['lat'][1:-1, :  ] # U-points (drop outside row)
-mom6_grid['supergrid']['lat'][ ::2,1::2] = roms_grid['v'  ]['lat'][ :  ,1:-1] # V-points (drop outside column)
+def convert_ROMS_to_MOM6(roms_grid):
+    """Convert the ROMS grid data into a skeleton MOM6 grid, mainly by
+    merging the four sets of point locations from the ROMS grid
+    into a single supergrid for MOM6."""
 
-mask = roms_grid['rho']['mask'][1:-1,1:-1]
-depth = roms_grid['rho']['h'][1:-1,1:-1] * mask
-mom6_grid['depth'] = depth
+    # Double the size of the *trimmed* ROMS grid to merge the four
+    # sets of points.
+    num_rows, num_cols = roms_grid['rho']['lon'].shape
+    num_rows *= 2
+    num_cols *= 2
 
-# Approximate edge lengths as great arcs
-R = 6370.e3 # Radius of sphere
-lat = mom6_grid['supergrid']['lat']
-lon = mom6_grid['supergrid']['lon']
-mom6_grid['supergrid']['dx'][:,:] = R * Spherical.angle_through_center( (lat[ :,1:],lon[ :,1:]), (lat[:  ,:-1],lon[:  ,:-1]) )
-mom6_grid['supergrid']['dy'][:,:] = R * Spherical.angle_through_center( (lat[1:, :],lon[1:, :]), (lat[:-1,:  ],lon[:-1,:  ]) )
+    mom6_grid = dict()
+    mom6_grid['supergrid'] = dict()
 
-# Approximate angles using centered differences in interior
-mom6_grid['supergrid']['angle'][:,1:-1] = numpy.arctan( (lat[:,2:]-lat[:,:-2]) / ((lon[:,2:]-lon[:,:-2]) * numpy.cos(numpy.deg2rad(lat[:,1:-1]))) )
-# Approximate angles using side differences on left/right edges
-mom6_grid['supergrid']['angle'][:,   0] = numpy.arctan( (lat[:, 1]-lat[:,  0]) / ((lon[:, 1]-lon[:,  0]) * numpy.cos(numpy.deg2rad(lat[:,   0]))) )
-mom6_grid['supergrid']['angle'][:,  -1] = numpy.arctan( (lat[:,-1]-lat[:, -2]) / ((lon[:,-1]-lon[:, -2]) * numpy.cos(numpy.deg2rad(lat[:,  -1]))) )
+    # Store the size for later
+    mom6_grid['num_rows'] = num_rows
+    mom6_grid['num_cols'] = num_cols
 
-# Approximate cell areas as that of spherical polygon
-mom6_grid['supergrid']['area'][:,:] = R * R * Spherical.quad_area(lat, lon)
+    # Copy points from ROMS grid
+    for field in ['lon', 'lat']:
+        mom6_grid['supergrid'][field] = numpy.zeros((num_rows+1,num_cols+1))
+        mom6_grid['supergrid'][field][ ::2, ::2] = roms_grid['psi'][field] # outer
+        mom6_grid['supergrid'][field][1::2,1::2] = roms_grid['rho'][field] # inner
+        mom6_grid['supergrid'][field][1::2, ::2] = roms_grid[ 'u' ][field] # between e/w
+        mom6_grid['supergrid'][field][ ::2,1::2] = roms_grid[ 'v' ][field] # between n/s
+
+    mom6_grid['depth'] = roms_grid['rho']['h'] * roms_grid['rho']['mask']
+
+    return mom6_grid
+
+def approximate_MOM6_grid_metrics(mom6_grid):
+    """Fill in missing MOM6 supergrid metrics by computing best guess
+    values."""
+
+    num_rows = mom6_grid['num_rows']
+    num_cols = mom6_grid['num_cols']
+    lat = mom6_grid['supergrid']['lat']
+    lon = mom6_grid['supergrid']['lon']
+
+    # Declare shapes
+    mom6_grid['supergrid']['area']  = numpy.zeros((num_rows,  num_cols  ))
+    mom6_grid['supergrid']['dx']    = numpy.zeros((num_rows+1,num_cols  ))
+    mom6_grid['supergrid']['dy']    = numpy.zeros((num_rows,  num_cols+1))
+    mom6_grid['supergrid']['angle'] = numpy.zeros((num_rows+1,num_cols+1))
+
+    # Approximate edge lengths as great arcs
+    R = 6370.e3 # Radius of sphere
+    mom6_grid['supergrid']['dx'][:,:] = R * Spherical.angle_through_center( (lat[ :,1:],lon[ :,1:]), (lat[:  ,:-1],lon[:  ,:-1]) )
+    mom6_grid['supergrid']['dy'][:,:] = R * Spherical.angle_through_center( (lat[1:, :],lon[1:, :]), (lat[:-1,:  ],lon[:-1,:  ]) )
+
+    # Approximate angles using centered differences in interior, and side differences on left/right edges
+    cos_lat = numpy.cos(numpy.radians(lat))
+    mom6_grid['supergrid']['angle'][:,1:-1] = numpy.arctan( (lat[:,2:] - lat[:,:-2]) / ((lon[:,2:] - lon[:,:-2]) * cos_lat[:,1:-1]) )
+    mom6_grid['supergrid']['angle'][:, 0  ] = numpy.arctan( (lat[:, 1] - lat[:, 0 ]) / ((lon[:, 1] - lon[:, 0 ]) * cos_lat[:, 0  ]) )
+    mom6_grid['supergrid']['angle'][:,-1  ] = numpy.arctan( (lat[:,-1] - lat[:,-2 ]) / ((lon[:,-1] - lon[:,-2 ]) * cos_lat[:,-1  ]) )
+
+    # Approximate cell areas as that of spherical polygon
+    mom6_grid['supergrid']['area'][:,:] = R * R * Spherical.quad_area(lat, lon)
+
+    return mom6_grid
 
 def write_MOM6_supergrid(mom6_grid):
     """Save the MOM6 supergrid data into its own file."""
@@ -135,5 +170,11 @@ def write_MOM6_topography(mom6_grid):
         hdepth.units = 'm'
         hdepth[:] = mom6_grid['depth']
 
+# Expecting a ROMS grid file as input
+roms_grid_filename = 'CCS_7k_0-360_fred_grd.nc'
+roms_grid = read_ROMS_grid(roms_grid_filename)
+roms_grid = trim_ROMS_grid(roms_grid)
+mom6_grid = convert_ROMS_to_MOM6(roms_grid)
+mom6_grid = approximate_MOM6_grid_metrics(mom6_grid)
 write_MOM6_supergrid(mom6_grid)
 write_MOM6_topography(mom6_grid)
