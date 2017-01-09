40a32a7b1b3553294208299ba201ab279c1560ea
Bryan Raney <bryan.raney@rutgers.edu>
2017-01-09 11:18:27 -0500

    Now testing Supercritical (speherical=False), too

--- mom6_tools/grid_converter/do_tests.bash
+++ mom6_tools/grid_converter/do_tests.bash
@@ -20,7 +20,12 @@ function ensure_nc_files_match() {
     status=$?
 
     # Indicate if the data differs, without printing out every different value
-    nccmp --data --Tolerance=1e-5 --statistics --force --quiet "${NCFILE1}" "${NCFILE2}"
+    # NOTE: --Tolerance (captial T) value is in percentage points;
+    #       Allowing 0.051% difference allows the xgrid_area (in the
+    #       atmos/land/ocean tileXtile files) values from the original
+    #       'make_quick_mosaic' tool to match "close enough" to the
+    #       ones calculated from the supergrid
+    nccmp --data --Tolerance=0.051 --force "${NCFILE1}" "${NCFILE2}"
     status2=$?
 
     # For nccmp, a return status of 0 means identical; 1 means not
@@ -31,19 +36,27 @@ function ensure_nc_files_match() {
     fi
 
     if [ $status -eq 0 ] ; then
-	echo "PASSED"
+	echo "PASSED: ${NCFILE1} vs ${NCFILE2}"
     else
-	echo "FAILED ($status): ${NCFILE1} ${NCFILE2}"
+	echo "FAILED ($status): ${NCFILE1} vs ${NCFILE2}"
     fi
     return $status
 }
 
-GRID_FILE="../../../ROMS-Inputs/CCS1/grid/CCS_7k_0-360_fred_grd.nc"
-
 NC_FILES="ocean_hgrid.nc ocean_topog.nc ocean_mosaic.nc land_mask.nc ocean_mask.nc atmos_mosaic_tile1Xland_mosaic_tile1.nc atmos_mosaic_tile1Xocean_mosaic_tile1.nc land_mosaic_tile1Xocean_mosaic_tile1.nc mosaic.nc"
 
+CCS_GRID_FILE="../../../ROMS-Inputs/CCS1/grid/CCS_7k_0-360_fred_grd.nc"
+
 rm -f ${NC_FILES}
-./convert_ROMS_grid_to_MOM6.py "${GRID_FILE}"
+./convert_ROMS_grid_to_MOM6.py "${CCS_GRID_FILE}"
 for file in ${NC_FILES}; do
-    ensure_nc_files_match expected_output/${file} ./${file}
+    ensure_nc_files_match expected_CCS/${file} ./${file}
+done
+
+SUPERCRITICAL_GRID_FILE="expected_Supercritical/grid_Supercritical.nc"
+
+rm -f ${NC_FILES}
+./convert_ROMS_grid_to_MOM6.py "${SUPERCRITICAL_GRID_FILE}"
+for file in ${NC_FILES}; do
+    ensure_nc_files_match expected_Supercritical/${file} ./${file}
 done
