6ebbe6caa916ce450c53e141a9ff2d22ff1f2aa7
Bryan Raney <bryan.raney@rutgers.edu>
2017-01-11 10:57:55 -0500

    Update script to use converter and not old tools
    Updated the original "create_grid.bash" script, which calls some
    Python code and other tools, to just use the converter.  This
    illustrates how to use the converter without breaking the original
    script.

--- mom6_tools/grid_converter/create_grid.bash
+++ mom6_tools/grid_converter/create_grid.bash
@@ -2,16 +2,6 @@
 
 module load anaconda
 
-FRETOOLS=/t3/workdir/raphael/fre-nctools/mom/src/tools
+GRID_FILE="/t1/scratch/raphael/CCS1/CCS1-I/CCS_7k_0-360_fred_grd.nc"
 
-ln -s /t1/scratch/raphael/CCS1/CCS1-I/CCS_7k_0-360_fred_grd.nc .
-
-python CreateFMSgridTopo.py
-
-module unload anaconda
-
-module load intel/14.0.0
-module load netcdf/4.3.0_intel14.0.0
-
-$FRETOOLS/make_solo_mosaic/make_solo_mosaic --num_tiles 1 --dir ./ --mosaic_name ocean_mosaic --tile_file ocean_hgrid.nc
-$FRETOOLS/make_quick_mosaic/make_quick_mosaic --input_mosaic ocean_mosaic.nc --ocean_topog ocean_topog.nc
+./convert_ROMS_grid_to_MOM6.py "${GRID_FILE}"
