2a3ae2ad3b4650dbff4d9693581c22838d7a1c9f
Kate Hedstrom <kshedstrom@alaska.edu>
2017-07-18 13:40:06 -0800

    Delete some trailing whitespace.

--- examples/rivers/make_river_clim.py
+++ examples/rivers/make_river_clim.py
@@ -6,7 +6,7 @@ import pycnal
 import pycnal_toolbox
 
 
-# load 2-dimentional discharge data 
+# load 2-dimentional discharge data
 print('Load discharge data')
 nc_data = netCDF.Dataset('CI_runoff.nc', 'r')
 nc_rivers = netCDF.Dataset('Cook_Inlet_rivers.nc', 'a')
@@ -54,7 +54,7 @@ for t in range(nt):
 	        frac = 1.0/count[j,i]
 		k = rivers[j,i][n]
 		runoff[k] = frac*data[t,j,i]
-	        
+
 
     if t==180:
         sum180 = np.sum(runoff)
